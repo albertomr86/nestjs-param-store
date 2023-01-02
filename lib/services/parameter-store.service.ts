e0c9293aa146c0eb8e7e2dce6271cb30f0f51103
Vladyslav Zavalykhatko <vladhatko@gmail.com>
2023-01-02 13:42:51 +0100

    recursively retrieve all parameters

--- lib/services/parameter-store.service.ts
+++ lib/services/parameter-store.service.ts
@@ -6,11 +6,43 @@ import {
 } from '@aws-sdk/client-ssm';
 import { SSM_PS_CLIENT } from '../constants';
 
+type GetParametersByPathRecursive = {
+  Path: string;
+  parameters: Parameter[];
+  NextToken?: string;
+};
+
 @Injectable()
 export class ParameterStoreService {
   public constructor(
     @Inject(SSM_PS_CLIENT) private readonly client: SSMClient,
   ) {}
+
+  private async getParametersByPathRecursive({
+    Path,
+    parameters,
+    NextToken,
+  }: GetParametersByPathRecursive): Promise<Parameter[]> {
+    const getParameters = new GetParametersByPathCommand({
+      Path,
+      NextToken,
+      WithDecryption: true,
+    });
+
+    const { Parameters = [], NextToken: nextToken } = await this.client.send(
+      getParameters,
+    );
+
+    if (nextToken) {
+      return this.getParametersByPathRecursive({
+        Path,
+        parameters: [...parameters, ...Parameters],
+        NextToken: nextToken,
+      });
+    }
+
+    return [...parameters, ...Parameters];
+  }
 
   public async getParametersByPath(path: string): Promise<Parameter[]> {
     const getParameters = new GetParametersByPathCommand({
@@ -18,8 +50,9 @@ export class ParameterStoreService {
       Path: path,
     });
 
-    const { Parameters = [] } = await this.client.send(getParameters);
-
-    return Parameters;
+    return this.getParametersByPathRecursive({
+      Path: path,
+      parameters: [],
+    });
   }
 }
