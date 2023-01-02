77a188f1432720f601180889ec6db81871450280
Vladyslav Zavalykhatko <vladhatko@gmail.com>
2023-01-02 10:22:53 +0100

    reflect ssm types more closer

--- lib/services/config.service.ts
+++ lib/services/config.service.ts
@@ -2,61 +2,23 @@ import { Injectable, Inject } from '@nestjs/common';
 import { Parameter } from '@aws-sdk/client-ssm';
 import { PS_CONFIG_PARAMETERS } from '../constants';
 
-type Maybe<T> = T | null;
-
 @Injectable()
 export class PSConfigService {
-  private static readonly trutyValues = [
-    'true',
-    'True',
-    '1',
-    'y',
-    'yes',
-    'Yes',
-  ];
-
   public constructor(
     @Inject(PS_CONFIG_PARAMETERS) private readonly parameters: Parameter[],
   ) {}
 
-  public get<R extends string>(
-    name: string,
-    defaultValue: Maybe<R> = null,
-  ): Maybe<R> {
-    const found = this.parameters.find((param) => param.Name?.endsWith(name));
-
-    if (!found) {
-      return defaultValue;
-    }
-
-    return found.Value as R;
-  }
-
-  public getBool<R extends boolean>(
-    name: string,
-    defaultValue: Maybe<R> = null,
-  ): Maybe<R> {
-    const found = this.parameters.find((param) => param.Name?.endsWith(name));
-
-    if (!found) {
-      return defaultValue;
-    }
-
-    const isTrue = PSConfigService.trutyValues.includes(found.Value ?? '');
-
-    return isTrue as R;
-  }
-
-  public getNumber<R extends number>(
-    name: string,
-    defaultValue: Maybe<R> = null,
-  ): Maybe<R> {
-    const found = this.parameters.find((param) => param.Name?.endsWith(name));
+  get = (name: string, defaultValue?: string): string => {
+    const found = this.parameters.find(({ Name }) => Name?.endsWith(name));
 
     if (!found?.Value) {
-      return defaultValue;
+      if (defaultValue) {
+        return defaultValue;
+      }
+
+      throw new Error(`Parameter ${name} not found`);
     }
 
-    return +found.Value as R;
-  }
+    return found.Value;
+  };
 }
