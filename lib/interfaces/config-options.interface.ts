cdf7f33fb89b4cf3a1d83673ec940256dca0255b
Alberto Menendez Romero <albertomr86@gmail.com>
2023-09-23 00:31:51 -0400

    Support recursive option

--- lib/interfaces/config-options.interface.ts
+++ lib/interfaces/config-options.interface.ts
@@ -3,5 +3,6 @@ import { SSMClientConfig } from '@aws-sdk/client-ssm';
 export interface PSConfigOptions {
   ssmParamStorePath: string;
   ssmDecryptParams?: boolean;
+  ssmRecursive?: boolean;
   ssmClientOptions?: SSMClientConfig;
 }
